092a98daaccdfa1c16f74728345844b37474e576
shubhamSP2198 <84079792+shubhamSP2198@users.noreply.github.com>
2021-10-18 20:40:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,10 +13,8 @@ export default class App extends Component {
     }
   }
 
-  url = 'http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D'
-
   async componentDidMount() {
-    let response = await axios.get(this.url);
+    let response = await axios.get('http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D');
     this.setState({
       loader: false,
       data: response.data
